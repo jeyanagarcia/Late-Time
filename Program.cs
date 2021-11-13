1e847b3ce854a9f9b4c0900e86ef451c12eb10d4
Jay Ann Garcia <92573753+jeyanagarcia@users.noreply.github.com>
2021-11-13 16:13:04 +0800

    i upload the program file :)

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace LateHours
+{
+    class Program
+    {
+        public static String id = "", pass = "";
+        public static TimeSpan timeIn = new TimeSpan(0, 0, 0);
+        static void Main(string[] args)
+        {
+            String employeeId = "";
+            String employeePass = "";
+            TimeSpan timeOut = new TimeSpan(0, 0, 0);
+            TimeSpan gracePeriod = new TimeSpan(8, 30, 0);
+            TimeSpan totalHours = new TimeSpan(0, 0, 0);
+            TimeSpan regularHoursStart = new TimeSpan(8, 0, 0);
+            TimeSpan regularHoursEnd = new TimeSpan(17, 0, 0);
+            TimeSpan lateIn = new TimeSpan(0, 0, 0);
+            TimeSpan earlyOut = new TimeSpan(0, 0, 0);
+            TimeSpan totalLateHours = new TimeSpan(0, 0, 0);
+
+            Console.WriteLine("Welcome to Employee Time Keeping System");
+            Console.WriteLine($"Today's Date:  {DateTime.Today.ToShortDateString()}");
+            Console.WriteLine("Press 1: Time-In");
+            Console.WriteLine("Press 2: Time-Out");
+            Console.WriteLine("Enter the number: ");
+            String response = Console.ReadLine();
+
+            switch (response){
+                    case "1":
+                    Console.WriteLine("-------------------");
+                    Console.WriteLine("Please Log-in");
+
+                    Console.Write("Enter your Employee ID: ");
+                    id = Console.ReadLine();
+
+                    Console.Write("Enter your password: ");
+                    pass = Console.ReadLine();
+
+                    timeIn = new TimeSpan(8, 30, 0);
+
+                    Console.WriteLine("Log In Successfully");
+                    Console.WriteLine("\n \n \n");
+                    Main(args);
+                    break;
+
+                    case "2":
+                    Console.WriteLine("-------------------");
+                    Console.WriteLine("Please Log-in");
+
+                    Console.Write("Enter your Employee ID: ");
+                    employeeId = Console.ReadLine();
+
+                    Console.Write("Enter your password: ");
+                    employeePass = Console.ReadLine();
+
+                    if (employeeId.Equals(id))
+                    {
+                        if (employeePass.Equals(pass))
+                        {
+                            timeOut = new TimeSpan(17, 30, 0);
+
+                            Console.WriteLine($"Your log-out time is recorded: {timeOut}");
+                            TimeSpan lunchBreakDuration = new TimeSpan(1, 0, 0);
+                            totalHours = (timeOut - timeIn) - lunchBreakDuration;
+
+                            if (timeIn >= gracePeriod)
+                            {
+                                totalLateHours = (gracePeriod - regularHoursStart) + (gracePeriod - timeIn);
+                                Console.WriteLine($"Hi employee {employeeId}");
+                                Console.WriteLine($"Your total late hours is: {totalLateHours}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please time in first.");
+                                Console.WriteLine("\n \n \n");
+                                Main(args);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error (Incorrect Employee ID or Password");
+                            Console.WriteLine("\n \n \n");
+                            Main(args);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error (Incorrect Employee ID or Password" + employeeId + id);
+                        Console.WriteLine("\n \n \n");
+                        Main(args);
+                    }
+
+                    break;
+
+                        }
+                    }
+            }
+        }
